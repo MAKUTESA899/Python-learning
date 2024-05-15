a369e1c63aa062dc59617dcfbb6e48dcb2310797
makutesa899 <alexkamau899@gmail.com>
2024-05-15 03:40:18 -0700

    calculator

---
+++ loop.py
@@ -0,0 +1,14 @@
+#while loops
+#x = 1
+#while x < 11:
+    #print(x)
+    x +=1
+
+#the continue statement
+
+x = 0
+while x < 6:
+    x +=1
+    if x ==3:
+        continue
+    print(x)
--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-t = 1.2
-b = int(input("insert the base :"))
-h = int(input("insert the height :"))
-area = t / b * h
-print()
+#sum
+print("this is my calculator")
+choice = int(input("to exsit press 0 to continue press 1 \n choose your option: "))
+while choice >= 1:
+    operation = int(input("1.addition \n2.subtraction \n3.divison \n4.multiplication\n0 exist\noperation ="))
+    if (operation ==1):
+       a = int(input("a ;"))
+       b = int(input("b ;"))
+       result = a + b
+       print(f"the sum of {a} and {b} is {result}")
+    elif (operation ==2):
+        a = int(input("a ;"))
+        b = int(input("b ;"))
+        result = a - b
+        print(f"the subtraction of {a} and {b} is {result}")
+    elif (operation ==3):
+        a = int(input("a ;"))
+        b = int(input("b ;"))
+        result = a / b
+        print(f"the division of {a} and {b} is {result}")
+    elif (operation ==2):
+        a = int(input("a ;"))
+        b = int(input("b ;"))
+        result = a * b
+        print(f"the division of {a} and {b} is {result}") 
+    elif operation ==0:
+        choice =0
+        print ("HAVE A NICE DAY") 
+    else:
+        print("the insert does not exist") 
